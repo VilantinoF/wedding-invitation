638426c88b8ce2a586f6e00d014717783846cfb9
Vilantino Fernandion <vilantinof@pop-os.localdomain>
2023-05-21 03:50:17 +0700

    mengubah tanggal

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,6 +7,6 @@ export const GOOGLE_MAPS_LINK = `https://goo.gl/maps/uj8mZZYbhdt5ACRE8`;
 /**
  * Wedding time
  */
-export const EPOCH_START_EVENT = 1684828800;
-export const EPOCH_END_EVENT = 1684850400;
-export const UTC_WEDDING_TIME = '2023-05-23:03:00:00Z';
+export const EPOCH_START_EVENT = 1685084400;
+export const EPOCH_END_EVENT = 1685084400;
+export const UTC_WEDDING_TIME = '2023-05-26:03:00:00Z';
